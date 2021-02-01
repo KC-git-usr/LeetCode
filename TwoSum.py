4fe297970cef028a54379916fd712114df3cf1b5
KC <kumar.v.cube98@gmail.com>
2021-02-01 23:28:00 +0530

    Second commit of TwoSum

--- TwoSum.py
+++ TwoSum.py
@@ -1,16 +1,30 @@
-def twoSum(nums, target):
-    result = [0, 0]
-    flag = False
+'''
+Problem:
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+'''
+
+
+def b_twoSum(nums, target):
+    '''
+    brute force method
+    '''
     for i in range(0, len(nums)):
-        for j in range(i, len(nums)):
+        for j in range(i+1, len(nums)):
             if nums[i]+nums[j] == target:
-                result[0], result[1] = i, j
-                flag = True
-                break
-    if flag == True:
-        return result
-    else:
-        print("No match found")
+                return ([i, j])
 
 
-print(twoSum([3,2,4], 6))
+def h_twoSum(nums, target):
+    '''
+    single pass hash map
+    '''
+    h = {}
+    for i, num in enumerate(nums):
+        n = target - num
+        if n not in h:
+            h[num] = i
+        else:
+            return [h[n], i]
+
+print(h_twoSum([1,2,4], 3))
