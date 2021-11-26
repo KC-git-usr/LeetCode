b99afddaec00a986362ced888406670b65fc45e9
Kumar Ramesh <69975720+KC-git-usr@users.noreply.github.com>
2021-11-25 20:55:08 -0500

    Add files via upload

---
+++ Leetcode_one_liners.cpp
@@ -0,0 +1,15 @@
+//Leetcode one-liners
+
+nums.size()
+return *max_element(nums.begin(), nums.end());
+
+
+int arr[] = { 1, 5, 8, 9, 6, 7, 3, 4, 2, 0 };
+int n = sizeof(arr) / sizeof(arr[0]);
+
+
+
+//count freq of elements (used in counting sort)
+    // Store count of each character
+    for (i = 0; arr[i]; ++i)
+        ++count[arr[i]];
