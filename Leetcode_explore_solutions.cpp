779b0007cbf30dd3b521037a16d7012341d60fa7
Kumar Ramesh <69975720+KC-git-usr@users.noreply.github.com>
2021-11-30 09:59:14 -0500

    Add files via upload

--- Leetcode_explore_solutions.cpp
+++ Leetcode_explore_solutions.cpp
@@ -142,3 +142,35 @@ public boolean hasCycle(ListNode head)
     return false;
 }
 
+//BST
+//Pre-order traversal
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    void preorder(TreeNode* root, vector<int> &nodes)  //note the &
+    {
+        if(root==nullptr)
+            return;                                     //note the empty return
+        nodes.push_back(root->val);
+        preorder(root->left, nodes);
+        preorder(root->right, nodes);
+    }
+public:
+     
+    vector<int> preorderTraversal(TreeNode* root) 
+    {
+        vector<int> nodes;
+        preorder(root, nodes);
+        return nodes;
+    }
+};
