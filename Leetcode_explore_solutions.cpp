4bb7bc815a2df42b54404840e6bd2ce08fefb65f
Kumar Ramesh <69975720+KC-git-usr@users.noreply.github.com>
2021-11-30 21:37:42 -0500

    Add files via upload

--- Leetcode_explore_solutions.cpp
+++ Leetcode_explore_solutions.cpp
@@ -174,3 +174,37 @@ public:
         return nodes;
     }
 };
+
+
+//BFS
+//Level-order
+vector<vector<int>> levelOrder(TreeNode* root)
+{
+    vector<vector<int>> result;
+    if(!root)
+        return result;
+
+    queue<TreeNode*> s;
+    vector<int> level;
+    queue<TreeNode*> nodes;
+    s.push(root);
+    while(!s.empty())
+    {
+        TreeNode* n = s.front();
+        s.pop();
+        level.push_back(n->val);
+        if(n->left)
+            nodes.push(n->left);
+        if(n->right)
+            nodes.push(n->right);
+
+        if(s.empty())
+        {
+            s.swap(nodes);
+            result.push_back(level);
+            level.clear();
+        }
+    }
+    
+    return result;
+}
